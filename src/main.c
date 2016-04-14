edb5ec24a5c22be09b91a3135d0488e1020c4a25
scutar_n <nathan.scutari@epitech.eu>
2016-04-14 10:39:47 +0200

    alek

--- src/main.c
+++ src/main.c
@@ -5,15 +5,34 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Mon Apr  4 13:28:30 2016 Baptiste veyssiere
-** Last update Mon Apr  4 14:22:57 2016 Baptiste veyssiere
+** Last update Thu Apr 14 10:39:24 2016 nathan scutari
 */
 
 #include <stdio.h>
 #include "borwein.h"
 
+int	syntax_check(char *str)
+{
+  int	i;
+
+  i = -1;
+  while (str[++i] != 0)
+    if (str[i] < '0' || str[i] > '9')
+      {
+	printf("Error : argument must be a positive integer\n");
+	return (1);
+      }
+  return (0);
+}
+
 int	main(int ac, char **av)
 {
-  if (ac != 2)
+  if (ac < 2)
+    {
+      printf("Usage : ./110borwein x (x : positive integer)\n");
+      return (84);
+    }
+  if (syntax_check(av[1]))
     return (84);
   calc(atoi(av[1]));
   return (0);
